27df8bae7a6826f7398d6582a1a236c796e76609
willjc96 <williamjamescook@aol.com>
2022-09-18 15:10:04 +0100

    check if number of participants is odd or even to determine how to shift the player order

--- src/components/shiftPlayerOrder.js
+++ src/components/shiftPlayerOrder.js
@@ -1,11 +1,20 @@
 export const shiftPlayerOrder = (teamA, teamB) => {
+  const isEvenNumberOfPlayers = (teamA.length + teamB.length) % 2 == 0;
   const newTeamA = teamA;
   const newTeamB = teamB;
   const poppedValue = newTeamA.pop();
   newTeamB.push(poppedValue);
   const shiftedValue = newTeamB.shift();
-  newTeamA.splice(1, 0, shiftedValue);
+  isEvenNumberOfPlayers
+    ? newTeamA.splice(1, 0, shiftedValue)
+    : newTeamA.unshift(shiftedValue);
   teamA = newTeamA;
   teamB = newTeamB;
   return [teamA, teamB];
 };
+  // if even number of players then first player needs to remain in same position to face all players
+  // A = [1,2]  A = [1,3]
+  // B = [3,4]  B = [4,2]
+  // if odd number of players then all players can rotate as there will be a different player not paired each time
+  // A = [1,2,3]  A = [4,1,2]
+  // B = [4,5]    B = [5,3]
